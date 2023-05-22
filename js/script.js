fdcbe62a15f12aa0355f467bb30cf970360b5059
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-05-22 16:10:26 +0200

    palindrmo test

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const userWord = prompt ("insert a word");
+let reversWord = 
+
+function palindromo (check) {
+    if (userWord === reversWord) {
+    
+    return (true);
+    }
+}
+
+console.log (userWord);
+
+/**
+ * @param
+ * @returns
+ */
